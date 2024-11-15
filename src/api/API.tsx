fc4337f684e0235be5c8f1581ea9d5f99202af27
Keegan Hinton <khinton386@gmail.com>
2024-11-15 18:52:00 -0500

    uncommented console logs in api.tsx

--- src/api/API.tsx
+++ src/api/API.tsx
@@ -1,7 +1,9 @@
 const searchGithub = async () => {
   try {
     const start = Math.floor(Math.random() * 100000000) + 1;
-    // console.log(import.meta.env);
+    
+    console.log(import.meta.env);
+    
     const response = await fetch(
       `https://api.github.com/users?since=${start}`,
       {
@@ -10,15 +12,17 @@ const searchGithub = async () => {
         },
       }
     );
-    // console.log('Response:', response);
+    
+    console.log('Response:', response);
+    
     const data = await response.json();
     if (!response.ok) {
       throw new Error('invalid API response, check the network tab');
     }
-    // console.log('Data:', data);
+    console.log('Data:', data);
     return data;
   } catch (err) {
-    // console.log('an error occurred', err);
+    console.log('an error occurred', err);
     return [];
   }
 };
@@ -36,7 +40,7 @@ const searchGithubUser = async (username: string) => {
     }
     return data;
   } catch (err) {
-    // console.log('an error occurred', err);
+    console.log('an error occurred', err);
     return {};
   }
 };
