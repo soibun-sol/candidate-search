d2012d6abbbd6bccd0f5cc23eebcc28f7b456502
Keegan Hinton <khinton386@gmail.com>
2024-11-15 20:23:11 -0500

    updated nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,15 @@
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <div>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/potentialCandidate">Search Potential Candidates</a></li>
+          <li><a href="/savedCandidates">Saved Candidates</a></li>
+        </ul>
+      </nav>
+    </div>
   )
 };
 
